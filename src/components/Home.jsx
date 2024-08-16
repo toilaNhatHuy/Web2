baae2b7a1d9e50d7375edda17b8767857cf34c76
hello <nhoangphuc960@gmail.com>
2024-08-16 15:22:28 +0700

    Hello

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,9 @@ function Home() {
     return(
         <div className="home">
             <h1>HOME</h1>
-            <h2>Phucday</h2>
+            <h1>Phuc</h1>
+            <h1>Huy</h1>
+            <h1>Cuong</h1>
         </div>
     )
 }
