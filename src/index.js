79557f1f4d26359a70a602f75c141792cd024ca2
ToilaNhatHuy <nguyendinhnhathuy@gmail.com>
2024-08-16 16:00:17 +0700

    Hello

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 
 import reportWebVitals from './reportWebVitals';
 import Slideshow from './components/SlideShow';
-import Home from './components/Home'
+// import Home from './components/Home'
 import Slogan from './components/Slogan'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +13,6 @@ root.render(
   <React.StrictMode>
     <>
       <Navbar />
-      <Home/>
       <Slideshow/>
       <Slogan/>
       
