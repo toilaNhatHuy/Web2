0918f8f222a2e12dd5f1aa6ef192d309206a7652
ToilaNhatHuy <nguyendinhnhathuy@gmail.com>
2024-08-21 16:00:57 +0700

    About Us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import {Routes, Route, Link} from 'react-router-dom'
 import Home from "./Page/HomePage.jsx";
 import "./components/Navbar.css";
 import Product from './Page/ProductPage.jsx';
+import AboutUs from './Page/AboutusPage.jsx';
+import Signin from './Page/SigninPage.jsx';
 
 function App() {
     return (
@@ -24,6 +26,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<Product/>}/>
+                <Route path="/aboutus" element={<AboutUs/>}/>
+                <Route path="/signin" element={<Signin/>}/>
             </Routes>
         </div>
     )
