c517d1ed23a145ae6fd8fab766eedc11c0bd50b0
hello <nhoangphuc960@gmail.com>
2024-08-26 15:23:20 +0700

    update

---
+++ src/Page/SigninPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from '../components/SignIn_SigninPage'
+function Signin() {
+  return (
+    <>
+        <SignIn/>
+    </>
+  )
+}
+
+export default Signin
