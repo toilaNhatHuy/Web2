82a3120fbe3096c6bed3e7c06ab45423eec1b331
hello <nhoangphuc960@gmail.com>
2024-08-26 21:45:18 +0700

    merge code main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,12 +13,12 @@ function Footer(){
                 </div>
                 <div className="footer-info">
                     <div className="footer-slogan">
-                        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/454887513_342717868807516_7391926271869795433_n.png?_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=TZNlTc0-W_cQ7kNvgGno8Jr&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGbzXtD35PV_RgcX4cf5Lm8CHUO4CSKkWF-eaDti3HiEA&oe=66EBB6E2" id='streetbrand-footer' width={'250px'} height={'65px'} alt="" />
+                        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/454887513_342717868807516_7391926271869795433_n.png?_nc_cat=110&ccb=1-7&_nc_sid=0024fc&_nc_ohc=TZNlTc0-W_cQ7kNvgGno8Jr&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGbzXtD35PV_RgcX4cf5Lm8CHUO4CSKkWF-eaDti3HiEA&oe=66EBB6E2" id='streetbrand-footer' width={'250px'} height={'65px'} alt="a" />
                         <h1 id = "myh1">THE<br/> “EARTH”<br/> WITHOUT<br/> “ART” IS<br/> JUST “EH”</h1>
                     </div>
 
                     <div className="footer-payments">
-                        <img src="http://pluspng.com/img-png/payment-method-png-payment-gateways-600.png" width={'400px'} height={'380px'} alt="" />
+                        <img src="http://pluspng.com/img-png/payment-method-png-payment-gateways-600.png" width={'400px'} height={'380px'} alt="a" />
                     </div>
 
                     <div className="contact-us">
@@ -26,10 +26,10 @@ function Footer(){
                             <h2 id ="myh2">FOLLOW AND<br/> CONTACT US</h2>
                         </div>
                         <div className="ctu-icon">
-                            <i id = 'contact-icon' class="fa-brands fa-facebook"></i>
-                            <i id = 'contact-icon' class="fa-brands fa-instagram"></i>
-                            <i id = 'contact-icon' class="fa-brands fa-telegram"></i>
-                            <i id = 'contact-icon' class="fa-brands fa-facebook-messenger"></i>
+                            <i id = 'contact-icon' className="fa-brands fa-facebook"></i>
+                            <i id = 'contact-icon' className="fa-brands fa-instagram"></i>
+                            <i id = 'contact-icon' className="fa-brands fa-telegram"></i>
+                            <i id = 'contact-icon' className="fa-brands fa-facebook-messenger"></i>
                         </div>
                         <div className="ctu-clothes">
                             <div className="row-1">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar(){
     return(
             <div className="navbar">
                 <ul>
-                    <li><img id="logo"></img></li>
+                    <li><img id="logo"alt="a"></img></li>
                     <li><Link id="home" to="/">HOME</Link></li>
                     <li><Link id = "products" to="/products">PRODUCTS</Link></li>
                     <li><Link id = "about-us" to="/aboutus">ABOUT US</Link></li>
@@ -13,7 +13,7 @@ function Navbar(){
                     <li><Link id = "sign-up" to="/signup">SIGN UP</Link></li>
                     <div className="search-bar">
                         <li><a href="http://localhost:3000"><input id="search" type="text" placeholder='Search...'/></a></li>
-                        <button id = "search-icon"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        <button id = "search-icon"><i className="fa-solid fa-magnifying-glass"></i></button>
                     </div>
                     <li><Link id="cart" to="#"><i className="fa-solid fa-cart-shopping"></i></Link></li>
                 </ul>
