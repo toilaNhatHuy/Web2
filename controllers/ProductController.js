172f95c8fb36503b6dc1bfd1c1f913e31e84b9fe
hello <nhoangphuc960@gmail.com>
2024-08-26 15:11:56 +0700

    Sort và filter cho Product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -63,8 +63,8 @@ const getDetailsProduct = async (req,res) => {
 
 const getAllProduct = async (req,res) => {
     try{
-        const {limit, page, sort} = req.query
-        const response = await ProductService.getAllProduct(Number(limit) || 9, Number(page) || 0, sort)
+        const {limit, page, sort, filter} = req.query
+        const response = await ProductService.getAllProduct(Number(limit) || 9, Number(page) || 0, sort,filter)
         return res.status(200).json(response)
     }catch(e){
         return res.status(404).json({
