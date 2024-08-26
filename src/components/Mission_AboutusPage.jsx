5cbbc50787bd7c3b413c4edbff10efbc77d4b57d
hello <nhoangphuc960@gmail.com>
2024-08-26 15:30:17 +0700

    Merge

--- src/components/Mission_AboutusPage.jsx
+++ src/components/Mission_AboutusPage.jsx
@@ -28,32 +28,4 @@ function Mission() {
   )
 }
 
-export default Missionimport './Mission_AboutusPage.css'
-
-import React from 'react'
-
-function Mission() {
-  return (
-    <div className="Our-Mission">
-        <div className="Mission">
-            <h1 id = "mission-1">OUR MISSION</h1>
-            <p id = "afterword">Lorem Ipsum is simply dummy text of<br/> the printing and typesetting industry.<br/> Lorem Ipsum has been the industry's<br/> standard scdsc dummy Lorem Ipsum is<br/> seen the industry's standard<br/> dummy typesetting industry. Lorem<br/> Ipsum has been thetypesetting industry.<br/> Lorem Ipsum has been the</p>
-        </div>
-        <div className="Mission-Img">
-            <div className="M-img-1-2">
-                <div className="M-img-1">
-
-                </div>
-                <div className="M-img-2">
-
-                </div>
-            </div>
-            <div className="M-img-3">
-
-            </div>
-        </div>
-    </div>
-  )
-}
-
 export default Mission
